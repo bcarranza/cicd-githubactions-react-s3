c120a0104422ad8cef5bcc427919b62197a9ea53
bcarranza <bayron.carranza@gmail.com>
2023-09-18 00:01:10 -0600

    first commit

---
+++ infra/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = "~> 0.14"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+provider "aws" {
+  alias  = "acm_provider"
+  region = "us-east-1"
+}
---
+++ infra/variables.tf
@@ -0,0 +1,8 @@
+variable "bucket_name" {
+  type        = string
+  description = "The name of the bucket without the www. prefix. Normally domain_name."
+}
+
+variable "common_tags" {
+  description = "Common tags you want applied to all components."
+}
