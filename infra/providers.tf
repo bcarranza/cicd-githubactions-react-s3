9f5020f0bb7bf707bce745b40a0dce5d6af3a68f
EM-ivanpena <ivandejesus@gmail.com>
2023-10-27 01:32:29 -0700

    Validate Terraform version

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,5 +1,6 @@
 terraform {
-  required_version = "1.5.3"
+  #Change to latest version(1.6.2),previous version (1.5.3) triggers an unsopported Terraform version
+  required_version = "1.6.2"
 
   required_providers {
     aws = {
